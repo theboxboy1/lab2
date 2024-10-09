1c8a66835cfb56ac96d79698843b3308d2774172
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2024-10-08 23:40:13 -0400

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,7 @@
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+sum, difference, product, quotient = num_1+num_2, num_1-num_2, num_1*num_2, num_1/num_2
+
+print(f"The sum, difference, product, and quotient are: {sum, difference, product, round(quotient, 2)}")
